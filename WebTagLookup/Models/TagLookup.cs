ca608bd77ae547d8f7bfee62f50a8cc30fee7c68
jadounm <jadounm@hillsboroughcounty.org>
2017-06-27 11:15:52 -0400

    Update TagLookup.cs

--- WebTagLookup/Models/TagLookup.cs
+++ WebTagLookup/Models/TagLookup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,6 +6,7 @@ using System.Web;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace WebTagLookup.Models
 {
@@ -13,13 +14,14 @@ namespace WebTagLookup.Models
     {
         [Display(Name="License")]
         [RequireOne("license,micro", ErrorMessage = "Must enter either license or microchip number")]
-        [MinLength(6, ErrorMessage="License must be at least 6 characters long")]
+        [MinLength(6, ErrorMessage="License must be at least 6 characters")]
         public string license { get; set; }
+
         [Display(Name="Microchip")]
         [MinLength(9, ErrorMessage="Microchip must be at least 9 characters")]
         public string micro { get; set; }
 
-        public List<TagData> getTagInfo()
+        public string getTagInfo()
         {
             string license = this.license;
             string micro = this.micro;
@@ -29,55 +31,40 @@ namespace WebTagLookup.Models
             string constr = ConfigurationManager.ConnectionStrings["animal"].ToString();
             string SQL = "";
 
-            if (!string.IsNullOrEmpty(license) && !string.IsNullOrEmpty(micro))
+            if (!string.IsNullOrEmpty(micro))
             {
-                SQL = "SELECT TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
-                         "FROM [Animal].[SYSADM].WebTagLookUp WHERE TagNumber like '%" + license + "%' AND tattoo = '" + micro + "'";
+                SQL = "SELECT TOP 1 TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
+                         "FROM [Animal].[SYSADM].WebTagLookUp WHERE TagNumber = '" + micro + "' AND TagType = 'Microchip' OR tattoo = '" + micro + "'";
             }
             else if (!string.IsNullOrEmpty(license) && string.IsNullOrEmpty(micro))
             {
-                SQL = "SELECT TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
-                         "FROM [Animal].[SYSADM].WebTagLookUp WHERE TagNumber like '%" + license + "%'";
-            }
-            else if (string.IsNullOrEmpty(license) && !string.IsNullOrEmpty(micro))
-            {
-                SQL = "SELECT TOP 1 TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
-                         "FROM [Animal].[SYSADM].WebTagLookUp WHERE tattoo = '" + micro + "'";
-            }
+                if (license.Length == 6)
+                {
+                    SQL = "SELECT TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
+                             "FROM [Animal].[SYSADM].WebTagLookUp WHERE tagtail = '" + license + "'";
+                }
+                else
+                {
+                    SQL = "SELECT TagNumber, tattoo, FirstName + ' ' + LastName as OwnerName, Phone, PetName, PetType, Breed, Color, CONVERT(varchar,TagExpire,101) AS TagExpDate, CONVERT(varchar,VacExpire,101) AS VacExpDate " +
+                             "FROM [Animal].[SYSADM].WebTagLookUp WHERE TagNumber like '%" + license + "%'";
+                }
+            }            
 
             SQL = SQL + " ORDER BY TagExpire DESC";
 
             dsTag = selectRows(constr, SQL);
 
-            tdList = fillModel(dsTag);
+            return JsonConvert.SerializeObject(dsTag);
 
-            return tdList;
-        }        
+            //tdList = fillModel(dsTag);
 
-        public string tagInfoToHTML(List<TagData> tdList)
-        {
-            string html = "<table class=\"table table-bordered table-condensed table-hover\"><tr><th>Licnese #</th><th>Microchip #</th><th>Owner Name</th><th>Phone Number</th><th>Pet Name</th><th>Animal Type</th><th>Breed</th><th>Tag Expiration Date</th></tr>";
+            //return JsonConvert.SerializeObject(tdList);
 
-            foreach (TagData td in tdList)
-            {
-                html = html + "<tr>";
-
-                foreach (var item in td)
-                {
-                    html = html + "<td>" + item.ToString() + "</td>";
-                }
-
-                html = html + "</tr>";
-            }
-
-            html = html + "</table>";
-
-            return html;
         }
 
         private static DataSet selectRows(string connectionString, string queryString)
         {
-            using (SqlConnection connection = new SqlConnection(Environment.ExpandEnvironmentVariables(connectionString)))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 if (connection.State == ConnectionState.Closed)
                 {
@@ -91,32 +78,32 @@ namespace WebTagLookup.Models
             }
         }
 
-        private static List<TagData> fillModel(DataSet ds)
-        {
-            List<TagData> tdList = new List<TagData>();
+        //private static List<TagData> fillModel(DataSet ds)
+        //{
+        //    List<TagData> tdList = new List<TagData>();
 
-            if (ds.Tables[0].Rows.Count > 0)
-            {
-                foreach (DataRow row in ds.Tables[0].Rows)
-                {
-                    TagData td = new TagData();
+        //    if (ds.Tables[0].Rows.Count > 0)
+        //    {
+        //        foreach (DataRow row in ds.Tables[0].Rows)
+        //        {
+        //            TagData td = new TagData();
 
-                    td.TagNo = row["TagNumber"].ToString();
-                    td.MicroNo = row["tattoo"].ToString();
-                    td.OwnerName = row["OwnerName"].ToString();
-                    td.Phone = row["Phone"].ToString();
-                    td.PetName = row["PetName"].ToString();
-                    td.PetType = row["PetType"].ToString();
-                    td.Breed = row["Breed"].ToString();
-                    td.PetColor = row["Color"].ToString();
-                    td.VacExpDate = row["VacExpDate"].ToString();
-                    td.TagExpDate = row["TagExpDate"].ToString();
+        //            td.TagNo = row["TagNumber"].ToString();
+        //            td.MicroNo = row["tattoo"].ToString();
+        //            td.OwnerName = row["OwnerName"].ToString();
+        //            td.Phone = row["Phone"].ToString();
+        //            td.PetName = row["PetName"].ToString();
+        //            td.PetType = row["PetType"].ToString();
+        //            td.Breed = row["Breed"].ToString();
+        //            td.PetColor = row["Color"].ToString();
+        //            td.VacExpDate = row["VacExpDate"].ToString();
+        //            td.TagExpDate = row["TagExpDate"].ToString();
                     
-                    tdList.Add(td);
-                }
-            }
+        //            tdList.Add(td);
+        //        }
+        //    }
 
-            return tdList;
-        }
+        //    return tdList;
+        //}
     }
 }
