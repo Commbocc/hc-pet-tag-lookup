d5940c5767391a04e674459879c4290077e8fd7d
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-04-06 10:21:49 -0400

    npm update & remove router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
 import store from './store'
 
 import VueResource from 'vue-resource'
@@ -13,7 +12,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
 	el: '#app',
-	// router,
 	store,
 	template: '<App/>',
 	components: { App }
